4c1f90401992608624e03839b9f7bfabd09148d3
Mark Jansen <markelmar2002@gmail.com>
2020-09-02 17:39:16 +0200

    Index now shows all applications

---
+++ views/all_applications.php
@@ -0,0 +1,22 @@
+<title>Solicitatie Overzicht</title>
+<table class="table-dark table table-responsive">
+    <tr>
+        <th>Bedrijfsnaam:</th>
+        <th>Contact Persoon:</th>
+        <th>Sollicitatie Datum:</th>
+        <th>Sollicitatie Manier:</th>
+        <th>Aangenomen</th>
+        <th>&nbsp;</th>
+    </tr>
+<?php
+foreach ($all_applications as $application)
+{
+    $application = $application[0];
+    echo "<tr>";
+    echo "<td>" . $application->company . "</td>";
+    echo "<td>" . $application->contact . "</td>";
+    echo "<td>" . $application->date . "</td>";
+    echo "<td>" . $application->way . "</td>";
+    echo "<td>" . $application->accepted . "</td>";
+    echo "<td><a href='./?id=" . $application->getId() . "'> Details</a></td>";
+}
